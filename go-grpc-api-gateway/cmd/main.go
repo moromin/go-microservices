6dfa21757c953240011b90c4bae19ec700d0d612
moromin <34294957+moromin@users.noreply.github.com>
2022-04-01 15:48:09 +0900

    feat: API gateway main

--- go-grpc-api-gateway/cmd/main.go
+++ go-grpc-api-gateway/cmd/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
+	"github.com/moromin/go-grpc-api-gateway/pkg/auth"
 	"github.com/moromin/go-grpc-api-gateway/pkg/config"
+	"github.com/moromin/go-grpc-api-gateway/pkg/order"
+	"github.com/moromin/go-grpc-api-gateway/pkg/product"
 )
 
 func main() {
 	// check config
 	c, err := config.LoadConfig()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("Failed at config", err)
 	}
-	fmt.Printf("%#v\n", c)
+
+	r := gin.Default()
+
+	authSvc := *auth.RegisterRoutes(r, &c)
+	product.RegisterRoutes(r, &c, &authSvc)
+	order.RegisterRoutes(r, &c, &authSvc)
+
+	r.Run(c.Port)
 }
